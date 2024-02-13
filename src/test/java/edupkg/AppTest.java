d31e7a5edd0901d9a60d514d1d1921c059cd32a4
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2024-02-13 09:26:35 +0530

    Add files via upload

---
+++ src/test/java/edupkg/AppTest.java
@@ -0,0 +1,20 @@
+package edupkg;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
